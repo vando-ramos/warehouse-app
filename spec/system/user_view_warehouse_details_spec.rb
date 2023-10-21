83a5dc348d0fa922852060bf1260d7730b3d0261
vando <vandorramos@gmail.com>
2023-10-20 23:44:49 -0300

    Details of a warehouse

---
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'User sees details of a warehouse' do 
+  it 'and sees additional information' do
+    # Arrange
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                    address: 'Av do Aeroporto, 1000', cep: '15000-000',
+                    description: 'Warehouse for international cargo')
+
+    # Act
+    visit('/')
+    click_on('Aeroporto SP')
+
+    # Assert
+    expect(page).to have_content('Aeroporto SP')
+    expect(page).to have_content('Code: GRU')
+    expect(page).to have_content('City: Guarulhos')
+    expect(page).to have_content('Area: 100000 m2')
+    expect(page).to have_content('Address: Av do Aeroporto, 1000')
+    expect(page).to have_content('CEP: 15000-000')
+    expect(page).to have_content('Description: Warehouse for international cargo')
+  end
+end
