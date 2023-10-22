08d5688312d4ec0d8577925772b77570c6a630d5
vando <vandorramos@gmail.com>
2023-10-21 21:43:15 -0300

    Unit tests

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    context 'presence' do
+      it 'false when name is empty' do
+        # Arrange
+        warehouse = Warehouse.new(name: '', code: 'UNT', city: 'Test', area: 1,
+                                  address: 'Unit test, 1', cep: '15000-000', description: 'Unit test')
+
+        # Act
+        # result = warehouse.valid?
+
+        # Assert
+        # expect(result).to eq false
+        expect(warehouse).not_to be_valid
+      end
+    end
+
+    it 'false when code is already in use' do
+      # Arrange
+      first_warehouse = Warehouse.create(name: 'Unit', code: 'UNT', city: 'Unit', area: 1,
+                                          address: 'Unit, 1', cep: '15000-000', description: 'Unit')
+
+      second_warehouse = Warehouse.create(name: 'Test', code: 'UNT', city: 'Test', area: 2,
+                                          address: 'Test, 2', cep: '25000-000', description: 'Test')
+
+      # Act
+      # result = second_warehouse.valid?
+
+      # Assert
+      # expect(result).to eq false
+      expect(second_warehouse).not_to be_valid
+    end
+  end
 end
