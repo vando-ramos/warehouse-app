19c53dea7d7238916e4433e043567c3c8bf73764
vando <vandorramos@gmail.com>
2023-10-30 13:52:59 -0300

    User visit only own orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,6 +27,9 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    if @order.user != current_user
+      redirect_to root_path, alert: "You don't have access to this order!"
+    end
   end
 
   def search
--- spec/system/orders/user_view_own_orders_spec.rb
+++ spec/system/orders/user_view_own_orders_spec.rb
@@ -39,4 +39,58 @@ describe 'User sees own orders' do
     expect(page).not_to have_content(order2.code)
     expect(page).to have_content(order3.code)
   end
+
+  it 'and visits an order' do
+    # Arrange
+    user = User.create!(name: 'Fran', email: 'fran@email.com', password: '654321')
+
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                                  address: 'Av do Aeroporto, 1000', cep: '15000-000',
+                                  description: 'Warehouse for international cargo')
+
+    supplier = Supplier.create!(corporate_name: 'Samsung ltda', brand_name: 'Samsung',
+                                registration_number: '123456789', address: 'Samsung Street, 100',
+                                city: 'Samsung City', state: 'SC', email: 'samsung@samsung.com')
+
+    order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, expected_delivery_date: 1.day.from_now)
+
+    # Act
+    login_as(user)
+    visit root_path
+    click_on 'My Orders'
+    click_on order.code
+
+    # Assert
+    expect(page).to have_content('Order Details')
+    expect(page).to have_content(order.code)
+    expect(page).to have_content('Destination Warehouse: GRU - Aeroporto SP')
+    expect(page).to have_content('Supplier: Samsung ltda')
+    date_format = 1.day.from_now.to_date
+    expect(page).to have_content("Expected Delivery Date: #{date_format}")
+  end
+
+  it "and doesn't visit orders from other users" do
+    # Arrange
+    user1 = User.create!(name: 'Fran', email: 'fran@email.com', password: '654321')
+    user2 = User.create!(name: 'Bob', email: 'bob@email.com', password: '121212')
+
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                                  address: 'Av do Aeroporto, 1000', cep: '15000-000',
+                                  description: 'Warehouse for international cargo')
+
+    supplier = Supplier.create!(corporate_name: 'Samsung ltda', brand_name: 'Samsung',
+                                registration_number: '123456789', address: 'Samsung Street, 100',
+                                city: 'Samsung City', state: 'SC', email: 'samsung@samsung.com')
+
+    order = Order.create!(user: user1, warehouse: warehouse, supplier: supplier, expected_delivery_date: 1.day.from_now)
+
+    # Act
+    login_as(user2)
+    visit order_path(order.id)
+
+    # Assert
+    expect(current_path).not_to eq(order_path(order.id))
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("You don't have access to this order")
+  end
 end
